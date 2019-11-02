d30677ae3adbf914095abe5e5a880be7978e25a4
Glitch (teoguso-compass) <none>
2019-11-02 15:54:09 +0000

    🚔🖼 Checkpoint ./js/app.js:8244143/1010:8244315/1163

--- js/app.js
+++ js/app.js
@@ -206,7 +206,7 @@
       }
 
       
-      var closestTree = [52.4931957, 13.4492738];
+      var closestTree = [52.1, 13.4];
       
 //       // Copied from Stackoverflow - cross your fingers :-)
 //       function distFrom(lat1, lng1, lat2, lng2) {
@@ -221,6 +221,44 @@
 
 //       return dist;
 //       }
+      function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
+      function distFrom(lat_A, lon_A, lat_B, lon_B) {
+        let earthRadius = 6371000; //meters
+        var delta_lat = lat_B - lat_A;
+        var delta_lon = lon_B - lon_A;
+        
+        delta_lat = delta_lat * Math.PI / 180;
+        delta_lon = delta_lon * Math.PI / 180;
+
+//         let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+//                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+//                Math.sin(dLng/2) * Math.sin(dLng/2);
+//         let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//         let dist = earthRadius * c;
+
+//       return dist;
+//       }
+        
       function distAtoB(lat_A, lon_A, lat_B, lon_B) {
         var delta_lat = lat_B - lat_A;
         var delta_lon = lon_B - lon_A;
@@ -243,7 +281,7 @@
       positionCurrent.hng = heading + adjustment;
       let treeAngle = angleAtoB(positionCurrent.lng, positionCurrent.lat, closestTree[0], closestTree[1]);
       
-      let treeDist = distAtoB(positionCurrent.lng, positionCurrent.lat, closestTree[0], closestTree[1]);
+      let treeDist = distance(positionCurrent.lng, positionCurrent.lat, closestTree[0], closestTree[1], "K");
       
       var nextTreeHeading = positionCurrent.hng + treeAngle;
       nextTreeHeading = nextTreeHeading < 0 ? 360 + nextTreeHeading : nextTreeHeading;
